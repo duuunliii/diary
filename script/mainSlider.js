2e22bc7ad83844d1da7e9e859f9d352a497c90c8
duuun <duuunliii@gmail.com>
2021-07-20 16:46:50 +0900

    🩹 Make slider's mouseDrag disabled

--- script/mainSlider.js
+++ script/mainSlider.js
@@ -4,6 +4,6 @@ var mainSlider = tns({
   navContainer: '.slider-nav-list',
   navAsThumbnails: true,
   arrowKeys: true, //방향키로 조정
-  mouseDrag: true, //마우스 드래그
+  // mouseDrag: true, //마우스 드래그
   preventScrollOnTouch: true, //모바일에서 드래그 했을 때 콘솔창에 뜨는 오류 없애줌
 })
